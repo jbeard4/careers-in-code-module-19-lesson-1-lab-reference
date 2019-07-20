2704dc8f5afb418429d85400ea61f545c1ab0f28
Jacob Beard <jake@jacobeanrnd.com>
2019-07-20 15:30:27 -0400

    Fix lint

--- tests/e2e.js
+++ tests/e2e.js
@@ -57,8 +57,7 @@ module.exports = {
     req.end()
   },
   directHttpCallFailure: function (test) {
-    const input = 'foo';
-    const expectedOutput = 2
+    const input = 'foo'
     const postData = querystring.stringify({
       number: input
     })
@@ -82,7 +81,7 @@ module.exports = {
         s += chunk
       })
       res.on('end', () => {
-        test.ok(new RegExp('Bad Request','i').test(s))
+        test.ok(new RegExp('Bad Request', 'i').test(s))
         test.done()
       })
     })
@@ -96,22 +95,21 @@ module.exports = {
     req.end()
   },
 
-  simulateUserInteractionSuccess : function (test) {
-
+  simulateUserInteractionSuccess: function (test) {
     const puppeteer = require('puppeteer');
 
     (async () => {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.goto(`http://${hostname}:${port}`, {waitUntil: 'networkidle2'});
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      await page.goto(`http://${hostname}:${port}`, { waitUntil: 'networkidle2' })
       await page.focus('input[name="number"]')
-      const keyboard = await page.keyboard;
-      keyboard.press('1');
+      const keyboard = await page.keyboard
+      keyboard.press('1')
       await Promise.all([page.waitForNavigation(), page.click('input[type="submit"]')])
-      const content = await page.content();
+      const content = await page.content()
       test.ok(new RegExp('New number: 2').test(content))
-      await browser.close();
+      await browser.close()
       test.done()
-    })();
+    })()
   }
 }
